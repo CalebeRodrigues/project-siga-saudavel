81eb3302547b73d359b75024bcbd2a3b152450fa
Calebe Rodrigues <calebebrodrigues@gmail.com>
2022-10-30 17:17:15 -0300

    Rotas Categoria

--- backend/routes.js
+++ backend/routes.js
@@ -3,6 +3,7 @@ const express = require("express");
 const route = express.Router();
 
 const UserController = require("./src/controllers/UserController");
+const CategoriaController = require("./src/controllers/CategoriaController");
 
 route.get('/', (req, res) => {
     res.send("Siga Saudavel - API");
@@ -19,6 +20,11 @@ route.put('/user/update/:id', UserController.update);
 route.delete('/user/delete/:id', UserController.deleteById);
 
 // Categorias
+route.get('/categoria/all', CategoriaController.findAll);
+route.get('/categoria/:id', CategoriaController.findById);
 
+route.post('/categoria/create', CategoriaController.create);
+
+route.put('/categoria/update/:id', CategoriaController.update);
 
 module.exports = route;
